3d4e10f0be2c32b235d792eca035c9cb511eb8f8
danyaaksenov <den06317520631752>
2021-05-07 18:03:10 +0300

    learn spring mvc

---
+++ src/main/java/com/aksenov/springcource/Controller.java
@@ -0,0 +1,12 @@
+package com.aksenov.springcource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello d!</h2>
+</body>
+</html>
