ec281f130a3cd18f33e622d60f6a5f035fe12bb3
danyaaksenov <den06317520631752>
2021-05-10 14:53:40 +0300

    learn DAO pattern

---
+++ src/main/java/com/aksenov/springcource/controllers/PeopleController.java
@@ -0,0 +1,33 @@
+package com.aksenov.springcource.controllers;
+
+import com.aksenov.springcource.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        //получение людей из ДАО
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        //получение 1 человека по его айди из ДАО и передать в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
