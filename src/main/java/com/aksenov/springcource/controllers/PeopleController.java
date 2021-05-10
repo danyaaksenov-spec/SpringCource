b99d7ec12c61a91c62435aabfccff5b95b6060b6
danyaaksenov <den06317520631752>
2021-05-10 16:50:44 +0300

    post запрос

--- src/main/java/com/aksenov/springcource/controllers/PeopleController.java
+++ src/main/java/com/aksenov/springcource/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package com.aksenov.springcource.controllers;
 
 import com.aksenov.springcource.dao.PersonDAO;
+import com.aksenov.springcource.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -29,5 +28,16 @@ public class PeopleController {
         //получение 1 человека по его айди из ДАО и передать в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
